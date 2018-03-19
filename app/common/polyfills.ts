e07967f997c77aeec94d5585f3c44d46391909d2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-19 08:44:41 +0100

    ok

--- app/common/polyfills.ts
+++ app/common/polyfills.ts
@@ -1,2 +1,1 @@
 import 'core-js/client/shim.js';
-import 'zone.js/dist/zone.js';
--- app/common/vendor.ts
+++ app/common/vendor.ts
@@ -1,7 +1,7 @@
+import 'zone.js/dist/zone.js';
+import 'rxjs';
+
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
-
-// RxJS
-import 'rxjs';
