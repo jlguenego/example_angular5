157fcdaf73478a8f73610f6c4ff533548849fad6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-20 11:52:42 +0100

    rxjs

---
+++ app/_prerequisites/03-rxjs/05-refcount/main.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+    console.log('start');
+    // var obs1 = Rx.Observable.of(1, 2, 3, 4, 5).delayWhen(n => Rx.Observable.interval(1000 * n).take(1));
+    var obs1 = Rx.Observable.interval(1000).take(5).map(n => n + 1);
+
+    var multicasted = obs1.multicast(new Rx.Subject()).refCount();
+
+    var s1, s2;
+
+    // These are, under the hood, `subject.subscribe({...})`:
+    s1 = multicasted.subscribe({
+        next: (v) => console.log('observerA: ' + v),
+        complete: () => console.log('observerA: complete'),
+    });
+    setTimeout(() => {
+        s2 = multicasted.subscribe({
+            next: (v) => console.log('observerB: ' + v),
+            complete: () => console.log('observerB: complete'),
+        });
+    }, 1200);
+
+    setTimeout(() => {
+        s1.unsubscribe();
+        console.log('s1 unsubscribed');
+    }, 2200);
+
+    setTimeout(() => {
+        s2.unsubscribe();
+        console.log('s2 unsubscribed');
+    }, 3200);
+
+})();
