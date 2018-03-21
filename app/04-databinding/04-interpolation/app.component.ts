8d71f099e037cb28f5f986962eca42b5b7600c1f
Maite THOMIAS <maite.thomias@gmail.com>
2018-03-21 15:32:12 +0100

    ok

--- app/04-databinding/04-interpolation/app.component.ts
+++ app/04-databinding/04-interpolation/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+<h1>Interpolation binding</h1>
+{{myName}}
+`,
 })
-export class AppComponent  { name = 'Angular !!!'; }
+export class AppComponent { myName = 'Angular'; }
--- app/04-databinding/04-interpolation/app.module.ts
+++ app/04-databinding/04-interpolation/app.module.ts
@@ -1,13 +1,12 @@
-import '../common/vendor';
-
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
