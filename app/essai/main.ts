9874a52bd8eb46d2e9dc861ce66461b92d5a7c58
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-21 18:10:09 +0100

    ok

--- app/essai/main.ts
+++ app/essai/main.ts
@@ -5,5 +5,3 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-
-console.log('essaixxx');
