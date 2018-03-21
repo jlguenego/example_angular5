ee7f4c269cb280b49981183e7bb681ec36730736
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-21 18:17:20 +0100

    ok

--- app/01-hello-world/main.ts
+++ app/01-hello-world/main.ts
@@ -1,11 +1,9 @@
-import './style.scss';
-
+import '../common/style.scss';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-// import { enableProdMode } from '@angular/core';
-
 import { AppModule } from './app.module';
 
+// import { enableProdMode } from '@angular/core';
 // enableProdMode();
-platformBrowserDynamic().bootstrapModule(AppModule);
 
+platformBrowserDynamic().bootstrapModule(AppModule);
 console.log('Hello world!');
--- app/essai/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  template: `<h1>Helloxxx {{name}}</h1>`,
-})
-export class AppComponent  { name = 'Angular !!!'; }
