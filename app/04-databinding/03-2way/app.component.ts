e38d16a2d2e06bb29e4cccf28a6b9178057bbf94
Maite THOMIAS <maite.thomias@gmail.com>
2018-03-21 15:23:21 +0100

    2ways binding

--- app/04-databinding/03-2way/app.component.ts
+++ app/04-databinding/03-2way/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+<h1>2 ways binding</h1>
+<input [(ngModel)]="myName">
+{{myName}}
+`,
 })
-export class AppComponent  { name = 'Angular !!!'; }
+export class AppComponent { myName = 'Angular'; }
