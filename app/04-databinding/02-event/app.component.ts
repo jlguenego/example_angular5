ea431339480714b1d2f0cf75b350d1da52da9b68
Maite THOMIAS <maite.thomias@gmail.com>
2018-03-21 15:14:51 +0100

    event binding

--- app/04-databinding/02-event/app.component.ts
+++ app/04-databinding/02-event/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+<h1>Event Binding</h1>
+<button (click)="myAlert()">Click me!</button>
+`,
 })
-export class AppComponent  { name = 'Angular !!!'; }
+export class AppComponent {
+  myAlert() {
+    window.alert('Hello event binding!');
+  }
+}
