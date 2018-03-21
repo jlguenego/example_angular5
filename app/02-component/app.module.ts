7c7f143076c004f988d76dba11b40cb2188a05ed
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-21 08:51:54 +0100

    ok

---
+++ app/02-component/app.module.ts
@@ -0,0 +1,13 @@
+import '../common/vendor';
+
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent }  from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
