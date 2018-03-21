3dfa747c1e9fec7c80afc557d957036b269c77db
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-21 20:03:08 +0100

    ok

---
+++ app/06-service/app.module.ts
@@ -0,0 +1,14 @@
+import '../common/vendor';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { JoinPipe } from './join.pipe';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent, JoinPipe],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
