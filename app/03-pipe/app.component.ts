e28495f72db96b09ff42abf1d7dad53990d78382
Maite THOMIAS <maite.thomias@gmail.com>
2018-03-21 14:58:35 +0100

    pipe

--- app/03-pipe/app.component.ts
+++ app/03-pipe/app.component.ts
@@ -2,6 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>List: {{myArray | join: ';'}}</h1>`,
+  template: `
+<h1>List: {{myArray | join: ';'}}</h1>
+<h1>Time: {{now | date}}</h1>
+
+`,
 })
-export class AppComponent  { myArray = [1, 2, 3, 'coucou']; }
+export class AppComponent { 
+  myArray = [1, 2, 3, 'coucou'];
+  now = new Date();
+ }
