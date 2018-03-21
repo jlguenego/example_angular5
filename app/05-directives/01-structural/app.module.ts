4d225cf3388e3eadb78235bf9d5b6056c576842b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-21 19:13:38 +0100

    ok

--- app/05-directives/01-structural/app.module.ts
+++ app/05-directives/01-structural/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { JLGTimesDirective } from './jlg-times.directive';
+import { JLGTimesDirective } from './jlgTimes.directive';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
