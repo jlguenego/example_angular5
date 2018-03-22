6ad6c070e667b10a0da045f6374b5e63369172f5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-22 20:04:30 +0100

    ok

--- app/10-decorators/02-HostListener/main.ts
+++
@@ -1,5 +0,0 @@
-import '../../common/style.scss';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
