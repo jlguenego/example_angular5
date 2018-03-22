ac379575bac876ba1bf3567d15c3a570e8b0b8e1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-22 17:34:54 +0100

    ok

--- app/00-prerequisite/03-rxjs/04-multicast/main.js
+++ app/00-prerequisite/03-rxjs/04-multicast/main.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
-    // var source = Rx.Observable.interval(100);
     var source = Rx.Observable.from([1, 2, 3]);
+    // var source = Rx.Observable.interval(100).map(c => c + 1).take(5);
     const subject = new Rx.Subject();
     console.log('subject', subject);
     var multicasted = source.multicast(subject);
