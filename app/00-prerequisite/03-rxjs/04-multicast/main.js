18c4cfae9850c46ec618fa511396a14bc662df56
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-22 17:32:44 +0100

    ok

--- app/00-prerequisite/03-rxjs/04-multicast/main.js
+++ app/00-prerequisite/03-rxjs/04-multicast/main.js
@@ -1,14 +1,19 @@
 (function () {
     'use strict';
+    // var source = Rx.Observable.interval(100);
     var source = Rx.Observable.from([1, 2, 3]);
-    var multicasted = source.multicast(new Rx.Subject());
+    const subject = new Rx.Subject();
+    console.log('subject', subject);
+    var multicasted = source.multicast(subject);
+    console.log('multicasted', multicasted);
 
     // These are, under the hood, `subject.subscribe({...})`:
     multicasted.subscribe({
         next: (v) => console.log('observerA: ' + v)
     });
     multicasted.subscribe({
-        next: (v) => console.log('observerB: ' + v)
+        next: (v) => console.log('observerB: ' + v),
+        complete: () => console.log('observerB: complete'),
     });
 
     // This is, under the hood, `source.subscribe(subject)`:
