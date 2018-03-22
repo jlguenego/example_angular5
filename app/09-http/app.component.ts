24db631e49666d5d2949c5ef2b912d5d51f84e59
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-22 18:42:44 +0100

    ok

--- app/09-http/app.component.ts
+++ app/09-http/app.component.ts
@@ -12,21 +12,25 @@ interface DataJson {
   selector: 'my-app',
   template: `
 <h1>Http</h1>
-<button (click)="onClick()">Click me!</button>
-<button (click)="onClick2()">Click me also!</button>
-<button (click)="onClick3()">Click me v3!</button>
+<button (click)="onClick()">Click me!</button><br>
+<button (click)="onClick2()">Click me also!</button><br>
+<button (click)="onClick3()">Click me v3!</button><br>
+<button (click)="onClick4()">Click me v4!</button><br>
 <button (click)="reset()">Reset</button>
 <div>{{message}}</div>
+<div>{{status}}</div>
 `,
 
 })
 export class AppComponent { 
 
+  status: string;
   message: string;
 
   constructor(private http: HttpClient) { }
 
   onClick() {
+    this.reset();
     console.log('call the JSON');
     this.http.get('./data.json').subscribe({
       next: (data) => {
@@ -38,6 +42,7 @@ export class AppComponent {
   }
 
   onClick2() {
+    this.reset();
     console.log('call the JSON the good old AngularJS way...');
     this.http.get('./data.json').toPromise().then((data) => {
       console.log('data', data);
@@ -46,6 +51,7 @@ export class AppComponent {
   }
 
   onClick3() {
+    this.reset();
     console.log('solve the typing issue');
     this.http.get<DataJson>('./data.json').subscribe({
       next: (data) => {
@@ -56,7 +62,21 @@ export class AppComponent {
     });
   }
 
+  onClick4() {
+    this.reset();
+    console.log('look at the full response');
+    this.http.get<DataJson>('./data.json', {observe: 'response'}).subscribe({
+      next: (response) => {
+        console.log('response', response);
+        this.message = response.body.content;
+        this.status = 'Status is ' + response.status;
+      },
+      error: e => console.error('error', e)
+    });
+  }
+
   reset() {
     this.message = '';
+    this.status = '';
   }
  }
