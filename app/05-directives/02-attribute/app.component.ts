d0dc15b3cf84532552cf892dfb0c5c0defc1103d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-22 16:19:41 +0100

    ok

--- app/05-directives/02-attribute/app.component.ts
+++ app/05-directives/02-attribute/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-<h1>Structural Directive</h1>
+<h1>Attribute Directive</h1>
 <span jlg-draggable>I am draggable</span>
 <span [jlg-draggable]>I am also draggable</span>
 <span [jlg-draggable]="false" >And me too!</span>
