c6d475ce0b04c4bbbb9bfa707e9b28b9ae11072b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-22 18:30:54 +0100

    http

--- app/09-http/app.component.ts
+++ app/09-http/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+// if you want to use toPromise() on an observable, just add this operator.
+import 'rxjs/add/operator/toPromise';
+
 @Component({
   selector: 'my-app',
   template: `
 <h1>Http</h1>
-<button (click)="onClick()">Click me!</button><button (click)="reset()">Reset</button>
+<button (click)="onClick()">Click me!</button>
+<button (click)="onClick2()">Click me also!</button>
+<button (click)="reset()">Reset</button>
 <div>{{message}}</div>
 `,
 
@@ -25,6 +30,14 @@ export class AppComponent {
       },
       error: e => console.error('error', e)
     });
+  }
+
+  onClick2() {
+    console.log('call the JSON the good old AngularJS way...');
+    this.http.get('./data.json').toPromise().then((data) => {
+      console.log('data', data);
+      this.message = data['content'];
+    }).catch(e => console.error('error', e));
   }
 
   reset() {
