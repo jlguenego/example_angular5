5cade5bb5512fe19dd4d396f0ef59f9dde6a6356
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-23 16:59:37 +0100

    ok

--- app/common/JLGModule/jlg-click.directive.ts
+++ app/common/JLGModule/jlg-click.directive.ts
@@ -5,24 +5,13 @@ import { Directive, Input, HostListener, Output, ElementRef } from '@angular/cor
 })
 export class JLGClickDirective {
 
-  constructor(private element: ElementRef) { 
-    console.log('jlg-click constructor');
-  }
-
-  ngOnInit() {
-    console.log('jlg-click init');
-  }
+  constructor(private element: ElementRef) { }
 
   @Input('jlg-click') jlgClick: () => Promise<any>;
 
   @HostListener('click') onClick() {
-    console.log('click', this.jlgClick);
     this.element.nativeElement.disabled = true;
-    this.jlgClick().then(() => {
-      console.log('ok');
-      this.element.nativeElement.disabled = false;
-    }).catch(() => {
-      console.log('ko');
+    this.jlgClick().catch(() => { }).then(() => {
       this.element.nativeElement.disabled = false;
     });
   }
