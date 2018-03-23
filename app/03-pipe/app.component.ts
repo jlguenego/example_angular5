bececec65b93b83f92ea5e9fdcff4e3118109df7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-23 18:37:51 +0100

    ok

--- app/03-pipe/app.component.ts
+++ app/03-pipe/app.component.ts
@@ -3,7 +3,7 @@ import { angularObservable } from '@jlg-example-angular-common/observable';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',,
+  templateUrl: './app.component.html',
 })
 export class AppComponent { 
   myArray = [1, 2, 3, 'coucou'];
