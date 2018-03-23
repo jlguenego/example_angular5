f5c367e714477e5a6a966915cc5e2ac8c45a5a09
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-23 11:39:01 +0100

    ok

--- app/10-rest/rest.service.ts
+++ app/10-rest/rest.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+const log = response => {
+  console.log('response', response);
+  return response;
+};
+
+export interface RestResponse {
+  content: Object
+}
+
+
 @Injectable()
 export class RestService {
 
@@ -17,32 +27,32 @@ export class RestService {
 
   retrieveAll(resourceName: string): Promise<Object> {
     console.log('retrieveAll');
-    return this.http.get(this.getUrl(resourceName)).toPromise();
+    return this.http.get(this.getUrl(resourceName)).toPromise().then(log);
   }
 
   deleteAll(resourceName: string): Promise<Object> {
     console.log('create');
-    return this.http.delete(this.getUrl(resourceName)).toPromise();
+    return this.http.delete(this.getUrl(resourceName)).toPromise().then(log);
   }
 
   create(resourceName: string, content: object): Promise<Object> {
     console.log('create');
-    return this.http.post(this.getUrl(resourceName), content).toPromise();
+    return this.http.post(this.getUrl(resourceName), content).toPromise().then(log);
   }
 
   retrieve(resourceName: string, id: number): Promise<Object> {
     console.log('retrieve ', id);
-    return this.http.get(this.getUrl(resourceName) + `/${id}`).toPromise();
+    return this.http.get(this.getUrl(resourceName) + `/${id}`).toPromise().then(log);
   }
 
   update(resourceName: string, id: number, content: object): Promise<Object> {
     console.log('update ', id);
-    return this.http.put(this.getUrl(resourceName) + `/${id}`, content).toPromise();
+    return this.http.put(this.getUrl(resourceName) + `/${id}`, content).toPromise().then(log);
   }
 
   delete(resourceName: string, id: number): Promise<Object> {
     console.log('delete ', id);
-    return this.http.delete(this.getUrl(resourceName) + `/${id}`).toPromise();
+    return this.http.delete(this.getUrl(resourceName) + `/${id}`).toPromise().then(log);
   }
 
 
