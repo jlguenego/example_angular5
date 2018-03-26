488a6b91f2fa1d9eb0d7c0f5b45e1132f4cb8edd
Maite THOMIAS <maite.thomias@gmail.com>
2018-03-26 14:22:10 +0200

    css

--- app/07-router/AppRouting/home.component.ts
+++ app/07-router/AppRouting/home.component.ts
@@ -4,6 +4,9 @@ import { Component } from '@angular/core';
   selector: 'home-view',
   template: `
 <h1>Welcome to Angular!</h1>
+<section>
+<a href="..">Go to parent</a>
+</section>
 `,
 })
 export class HomeComponent {
