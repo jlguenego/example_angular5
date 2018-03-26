f04ff752a8a553efb72b28eb78c61f36ae6c41f1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-26 16:31:18 +0200

    ok

--- app/04-databinding/01-property/app.component.ts
+++ app/04-databinding/01-property/app.component.ts
@@ -8,19 +8,7 @@ import { Component } from '@angular/core';
   <img [src]="niceImageUrl">
 </div>
 `,
-<<<<<<< HEAD
   styleUrls: ['./app.component.css'],
-=======
-  styles: [`
-img {
-  width: 400px;
-  animation: fade-in 5s;
-}
-div {
-  text-align: center;
-}
-`],
->>>>>>> 6ff5ec4a3bb52df257150f6644637e77356249b2
 })
 export class AppComponent {
   niceImageUrl = '../../assets/chateau.jpg';
