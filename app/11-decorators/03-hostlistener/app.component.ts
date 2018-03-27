b18680dd399c6ab207deee5b7d1f0a0a624b85d5
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-27 16:29:57 +0200

    ok

---
+++ app/11-decorators/03-hostlistener/app.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, HostListener, ElementRef } from '@angular/core';
+import { of } from 'rxjs/observable/of';
+
+interface HelloEvent {
+  message: string;
+}
+
+@Component({
+  selector: 'my-app',
+  template: `
+<header>Hostlistener</header>
+Message: {{message}}
+`,
+})
+export class AppComponent {
+  constructor(private element: ElementRef) {}
+  ngOnInit() {
+    console.log('AppComponent: init');
+    of('Coucou Angular').delay(1000).subscribe(n => {
+      console.log('create event');
+      const e = new CustomEvent<HelloEvent>('hello', { detail: { message: n } });
+      this.element.nativeElement.dispatchEvent(e);
+    });
+
+  }
+
+  @HostListener('hello', ['$event']) onHello(obj: CustomEvent<HelloEvent>) {
+    console.log('obj', obj);
+    this.message = obj.detail.message;
+  }
+
+  message: string;
+}
