0fd2dffd39007e01eb7ed98089e15f488334072c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-27 12:13:53 +0200

    life cycle

--- app/02-component/app/my-app.component.ts
+++ app/02-component/app/my-app.component.ts
@@ -1,7 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, DoCheck, OnInit, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'my-app',
   templateUrl: './my-app.component.html',
 })
-export class MyAppComponent { name = 'Angular !!!'; }
+export class MyAppComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  message = '';
+
+  on: boolean = true;
+
+  ngOnInit() {
+    // run as a post constructor
+    console.log('MyAppComponent: ngOnInit');
+    Observable.interval(1000).take(2).map(n => ['Angular', 'Angular is Good'][n]).subscribe(str => this.message = str);
+    Observable.of(false).delay(3000).subscribe(n => this.on = n);
+  }
+
+  ngOnChanges() {
+    // will never be run because there is no @Input.
+    console.log('MyAppComponent: ngOnChanges');
+  }
+
+  ngDoCheck() {
+    console.log('MyAppComponent: ngDoCheck, message=|%s|, on=|%s|', this.message, this.on);
+  }
+
+  ngAfterContentInit() {
+    console.log('MyAppComponent: ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('MyAppComponent: ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('MyAppComponent: ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('MyAppComponent: ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('MyAppComponent: ngOnDestroy');
+  }
+
+
+}
--- app/02-component/child/child.component.ts
+++ app/02-component/child/child.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'child',
   templateUrl: './child.component.html',
 })
-export class ChildComponent { }
+export class ChildComponent implements OnChanges, OnInit, OnDestroy {
+  @Input() message: string;
+
+  ngOnInit(): void {
+    // run after ngOnChanges
+    console.log('ChildComponent: ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ChildComponent: ngOnChanges', changes);
+  }
+
+  ngOnDestroy() {
+    console.log('ChildComponent: ngOnDestroy');
+  }
+}
