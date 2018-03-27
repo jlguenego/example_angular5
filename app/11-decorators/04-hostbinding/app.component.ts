4861182fada656b1efc40ca82fd83c1ca6606008
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-27 17:09:43 +0200

    hostbinding

---
+++ app/11-decorators/04-hostbinding/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, HostListener, ElementRef, HostBinding } from '@angular/core';
+import { of } from 'rxjs/observable/of';
+import { interval } from 'rxjs/observable/interval';
+
+interface HelloEvent {
+  message: string;
+}
+
+@Component({
+  selector: 'my-app',
+  template: `
+<header>Hostlistener</header>
+<span>Look I am changing my color...</span>
+
+`,
+  styles: [`
+:host.red span {
+  color: red;
+}
+
+:host.blue span {
+  color: blue;
+}
+`]
+})
+export class AppComponent {
+  constructor(private element: ElementRef) { }
+  @HostBinding('class') myClass: string;
+  ngOnInit() {
+    console.log('AppComponent: init');
+    interval(1000).map(n => n + 1).startWith(0)
+      .map(n => ['blue', 'red'][n % 2]).subscribe(n => {
+      console.log('n', n);
+      this.myClass = n;
+    });
+
+  }
+
+
+}
