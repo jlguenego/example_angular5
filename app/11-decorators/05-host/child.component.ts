eb6bc5dd9cdb58994a2f98033080192c4cb09349
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-03-28 08:43:03 +0200

    ok

--- app/11-decorators/05-host/child.component.ts
+++ app/11-decorators/05-host/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Host, Optional } from '@angular/core';
+import { Component, Host, Optional, Self, SkipSelf } from '@angular/core';
 import { JLGService, JLGChildService } from './jlg.service';
 
 @Component({
@@ -14,12 +14,25 @@ I am the child.
 })
 export class ChildComponent {
 
-  // @Host is like @Self but will never go to look for the service on the module providers.
+  // @Host will never get the module service
+  // @Host will get the parent service only if it is projected target with ng-content (light dom)
+  // you have to see "host" like the ":host" in Scoped CSS
   constructor(
+
     @Host()
     @Optional()
-  
-  private jlg: JLGService) {
-    console.log('JLGService', jlg);
+    private jlg: JLGService,
+
+    @Self() 
+    @Optional()
+    private jlg2: JLGService,
+
+    @SkipSelf() 
+    @Optional()
+    private jlg3: JLGService,
+  ) {
+    console.log('jlg host', jlg);
+    console.log('jlg self', jlg2);
+    console.log('jlg skipself', jlg2);
   }
 }
