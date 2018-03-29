120046c464041a596dd2971c73f75c5a64307f28
Maite THOMIAS <maite.thomias@gmail.com>
2018-03-29 08:55:23 +0200

    ok

--- app/13-migration/02-angular5/main.ts
+++
@@ -1,5 +0,0 @@
-import '@jlg-example-angular-common/style.scss';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
