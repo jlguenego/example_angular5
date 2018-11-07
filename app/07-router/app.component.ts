7d09bbbdf3a0bd9332d6e4a3ce76ff9f5d3ab3d4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-11-07 12:54:16 +0100

    routing template

--- app/07-router/app.component.ts
+++ app/07-router/app.component.ts
@@ -3,16 +3,7 @@ import './app.component.scss';
 
 @Component({
   selector: 'my-app',
-  template: `
-  <header>
-    <nav>
-    <a routerLink="/" routerLinkActive="active" [routerLinkActiveOptions]="{exact: true}">Home</a>
-    <a routerLink="/product/5" routerLinkActive="active">Product#5</a>
-    <a routerLink="/about" routerLinkActive="active">About</a>
-    </nav>
-  </header>
-  <router-outlet></router-outlet>
-`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
