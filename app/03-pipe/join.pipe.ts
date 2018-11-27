4455c749bca804cbe188e1ba03eceab2075b9ba0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-11-27 10:50:11 +0100

    ok

--- app/03-pipe/join.pipe.ts
+++ app/03-pipe/join.pipe.ts
@@ -1,13 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-/*
- * Raise the value exponentially
- * Takes an exponent argument that defaults to 1.
- * Usage:
- *   value | exponentialStrength:exponent
- * Example:
- *   {{ 2 | exponentialStrength:10 }}
- *   formats to: 1024
-*/
+
 @Pipe({ name: 'join' })
 export class JoinPipe implements PipeTransform {
     transform<T>(value: T[], separator: string): string {
