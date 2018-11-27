9333115601793a71b3f6891bf76ca4b41ac15d3c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-11-27 10:57:50 +0100

    ok

--- app/03-pipe/join.pipe.ts
+++ app/03-pipe/join.pipe.ts
@@ -2,7 +2,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'join' })
 export class JoinPipe implements PipeTransform {
-    transform<T>(value: T[], separator: string): string {
+    transform(value: [], separator: string): string {
         return value.join(separator);
     }
 }
